aa8484e8b2a2cdb9996a248a4cc55345215fca76
simone <simonegauli@gmail.com>
2024-09-05 00:56:54 +0200

    diocan

--- src/lib/run-at-specific-time.ts
+++ src/lib/run-at-specific-time.ts
@@ -6,9 +6,12 @@ export const runAtSpecificTime = (hour: number, minute: number, fn: Callback<voi
   date.setMinutes(minute);
   date.setSeconds(0);
 
-  if (date < new Date()) throw new Error('This time is already gone.');
+  if (date < new Date()) {
+    date.setDate(date.getDate() + 1);
+  }
 
   const timeUntilFunction = date.getTime() - Date.now();
+  if (timeUntilFunction < 0) throw new Error('Internal time error.');
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   setTimeout(fn, timeUntilFunction);
 };
