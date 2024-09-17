bb20c76d5cd4a2db20e88832eda9222e5b11e317
simone <simonegauli@gmail.com>
2024-09-17 23:53:35 +0200

    add test

---
+++ __tests__/coraline.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from '@jest/globals';
+
+describe('sendMessage', () => {
+  it('Nothing.', () => {
+    // eslint-disable-next-line no-console
+    console.log('No test');
+  });
+});
