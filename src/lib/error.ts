d43fbb363276fc0b3cc421133e35d27ff13919eb
simone <simonegauli@gmail.com>
2024-10-06 03:23:38 +0200

    improve error handler

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -2,18 +2,24 @@ export const errToString = (err: unknown, ...args: string[]) => {
   let error = '';
   if (err instanceof Error) {
     error += err.message;
-  }
-  if (typeof err === 'string') {
+  } else if (typeof err === 'string') {
     error += err;
-  }
-  if (typeof err === 'object' && err !== null && 'description' in err) {
+  } else if (typeof err === 'object' && err !== null && 'description' in err) {
     const des = err.description;
     if (typeof des === 'string') {
       error += des;
     }
+  } else {
+    try {
+      error = `UNHANDLED API ERROR: ${JSON.stringify(err)}`;
+    } catch {
+      error = 'UNHANDLED API ERROR';
+    }
   }
+
   for (const value of args) {
     error += ' ' + value;
   }
-  return error || 'API ERROR';
+
+  return error;
 };
