fe6b6925d7e3393937ee4881567709b1c5a3eee4
simone <simonegauli@gmail.com>
2024-11-01 23:34:21 +0100

    revert oneof

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,3 +1,1 @@
 export type Callback<T> = (() => Promise<T>) | (() => T);
-
-export type OneOf<T, K extends keyof T = keyof T> = K extends keyof T ? { [P in K]: T[P] } & Record<Exclude<keyof T, K>, never> : never;
