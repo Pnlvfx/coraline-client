97a76657e20f50d0890094e03abfbd3b0dff2021
simone <simonegauli@gmail.com>
2024-11-08 03:07:01 +0100

    improve regexs

--- src/lib/regex.ts
+++ src/lib/regex.ts
@@ -1,13 +1,19 @@
 export const regex = {
-  /** Match two strings using regex even if there are differences in upper or lower case */
-  upperLowerCase: (name: string) => new RegExp(`^${name}$`, 'i'),
   /** Detect if a given string has emoji. */
   detectEmoji: /<a?:.+?:\d{18}>|\p{Extended_Pictographic}/gu,
   /** Remove any emoji from a given string. */
   removeEmojis: (str: string) => str.replaceAll(/[^\p{L}\p{N}\p{P}\p{Z}^$\n]/gu, ''),
-  /** Remove parentheses and the content inside it from a given string. It only detect () parenthesis. */
-  // eslint-disable-next-line sonarjs/slow-regex
-  removeParentheses: (str: string) => str.replaceAll(/\s*\(.*?\)\s*/g, ''),
+  /** Remove parentheses and the content inside it from a given string.
+   * @credit https://github.com/joshunrau/ytdl-mp3/blob/main/src/utils.ts
+   */
+  removeParenthesizedText: (s: string) => {
+    // eslint-disable-next-line sonarjs/slow-regex
+    const regex = /\s*([([][^()[\]]*[)\]])\s*/g;
+    while (regex.test(s)) {
+      s = s.replaceAll(regex, '');
+    }
+    return s;
+  },
   /** Remove any accents and tranform it to it's relative character from a given string. Example: "Nèza" will return "Neza" */
   normalizeAccents: (str: string) => str.normalize('NFD').replaceAll(/\p{Diacritic}/gu, ''),
   /** Match a string even with some differences, but note that it might be incorrect. */
